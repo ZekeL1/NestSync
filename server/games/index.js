bd5bce622eb0322c6034158feb64eb2183e53bc2
Yan Liu <y666liu@uwaterloo.ca>
2026-03-26 14:06:15 -0400

    feat(games): add shared sudoku room state and sync

--- server/games/index.js
+++ server/games/index.js
@@ -1,7 +1,9 @@
 const { registerPictionaryHandlers } = require('./pictionary');
+const { registerSudokuHandlers } = require('./sudoku');
 
 function registerGameHandlers(io, socket) {
   registerPictionaryHandlers(io, socket);
+  registerSudokuHandlers(io, socket);
 }
 
 module.exports = {
